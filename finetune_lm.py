cbedb3764ada8f8e7c4ab00da59356b3b4fe2956
tanganke <tang.anke@foxmail.com>
2023-08-03 22:36:54 +0800

    finetune ml

--- finetune_lm.py
+++ finetune_lm.py
@@ -1,22 +1,68 @@
 """This script finetunes a pretrained language model on a text-to-text dataset."""
 import logging
 from pathlib import Path
+from typing import Any
 
 import hydra
 import lightning as L
 import lightning.pytorch as pl
+from lightning.pytorch.utilities.types import STEP_OUTPUT
 import peft
 import torch
 from datasets import load_dataset
 from hydra.utils import instantiate
 from omegaconf import DictConfig, OmegaConf
 from torch import Tensor, nn
+from torch.utils.data import DataLoader
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
 import peta
 from peta.utils.logging.rich import pprint_yaml, setup_colorlogging
+from peta.utils.ml.devices import num_devices
+from peta.utils import TitledLog, TimeIt
 
 log = logging.getLogger(__name__)
+
+
+class Seq2SeqLMModule(pl.LightningModule):
+    def __init__(
+        self,
+        model: AutoModelForSeq2SeqLM,
+        tokenizer: AutoTokenizer,
+        optim_cfg: DictConfig,
+    ):
+        super().__init__()
+        self.model = model
+        self.tokenizer = tokenizer
+        self.optim_cfg = optim_cfg
+
+    def configure_optimizers(self):
+        """
+        Configure the optimizer and learning rate scheduler.
+
+        Returns:
+            Dict: Dictionary containing the optimizer and learning rate scheduler.
+        """
+        optim = {}
+        if "optimizer" in self.optim_cfg:
+            optim["optimizer"]: torch.optim.Optimizer = instantiate(
+                self.optim_cfg["optimizer"],
+                params=self.parameters(),
+            )
+        if "lr_scheduler" in self.optim_cfg:
+            optim["lr_scheduler"]: torch.optim.lr_scheduler.LRScheduler = instantiate(
+                self.optim_cfg["lr_scheduler"],
+                optimizer=optim["optimizer"],
+            )
+        log.info(f"{'configure_optimizers':=^50}")
+        log.info(optim)
+        return optim
+
+    def training_step(self, batch, batch_idx):
+        outputs = self.model(**batch)
+        loss = outputs.loss
+        self.log("train/loss", loss)
+        return loss
 
 
 @hydra.main("config", "finetune_lm", None)
@@ -24,21 +70,40 @@ def main(cfg: DictConfig):
     setup_colorlogging(force=True)
     pprint_yaml(OmegaConf.to_yaml(cfg))
     if hasattr(cfg, "seed") and cfg.seed is not None:
+        log.info(f"set seed to {cfg.seed}")
         L.seed_everything(cfg.seed)
 
     # load pretrained model and tokenizer
-    tokenizer = instantiate(cfg.model.tokenizer)
+    with TitledLog("load pretrained model and tokenizer", log.info):
+        tokenizer = instantiate(cfg.model.tokenizer)
 
-    model = instantiate(cfg.model.model)
-    if cfg.peft.peft_config is not None:
-        peft_config: peft.PeftConfig = instantiate(cfg.peft.peft_config)
-        if hasattr(cfg.peft.seed) and cfg.peft.seed is not None:
-            L.seed_everything(cfg.peft.seed)
-        model = peft.get_peft_model(model, peft_config)
+        model = instantiate(cfg.model.model)
+        if cfg.peft.peft_config is not None:
+            peft_config: peft.PeftConfig = instantiate(cfg.peft.peft_config)
+            if hasattr(cfg.peft, "seed") and cfg.peft.seed is not None:
+                log.info(f"set peft seed to {cfg.peft.seed}")
+                L.seed_everything(cfg.peft.seed)
+            model = peft.get_peft_model(model, peft_config)
+            model.print_trainable_parameters()
+        else:
+            log.info(f"no peft config found, use full finetuning.")
+        module = Seq2SeqLMModule(model, tokenizer, optim_cfg=cfg.optim)
 
     # load dataset
-    datasets = instantiate(cfg.dataset.datasets)
+    with TitledLog("load datasets and dataloaders", log.info):
+        datasets = instantiate(cfg.dataset.datasets)
+        assert (
+            cfg.batch_size % num_devices(cfg.trainer.devices) == 0
+        ), "batch_size must be divisible by the number of devices."
+        batch_size = cfg.batch_size // num_devices(cfg.trainer.devices)
+        train_loader = DataLoader(
+            datasets["train"],
+            batch_size=batch_size,
+            shuffle=True,
+        )
 
+    trainer = pl.Trainer(**cfg.trainer)
+    trainer.fit(module, train_loader)
     exit(0)
 
 
